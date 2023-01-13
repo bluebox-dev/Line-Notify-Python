a6bfb08e4116ac6374802c7d7b358c8b42dd4793
Thanakorn Kriangudom <45796358+bluebox-dev@users.noreply.github.com>
2023-01-13 15:11:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ notify.send("แจ้งเตือนภัย")
 notify.send("คนร้าย", image_path='./1.png')
 
 
+## Function
+def SendTxt(str):
+    notify.send(str)
+    
+## Function
+def SendImage(txt, path):
+    notify.send(txt, image_path=path)
+
+## Add to yolov5
+# if int(cls)== 67:
+#    ln.SendTxt(int(cls))
+#    cv2.imwrite("img.jpg", imc)
+#    ln.SendImage(str(cls), "img.jpg"
